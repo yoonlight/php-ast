645e24bd043f79478b9eff71634e9d6f8f17b0e4
kjw <yoonlight12@gmail.com>
2022-07-26 13:12:38 +0900

    feat: add split Function & Method Declaration

--- app/AST/MSParser.php
+++ app/AST/MSParser.php
@@ -5,6 +5,7 @@ namespace App\AST;
 use App\FeatureEntities\ProgramFeatures;
 use Microsoft\PhpParser\Parser;
 use Microsoft\PhpParser\Node;
+ini_set('memory_limit','-1');
 
 class MSParser
 {
@@ -13,32 +14,102 @@ class MSParser
     public $upSymbol = "^";
     public $downSymbol = "_";
     private $emptyString = "";
+    /** @var Node[] */
+    private $methods;
+    /** @var Node[] */
+    private $mainMethod;
 
     public function extractFeatures($code)
     {
         $methods = $this->parse($code);
-        $result = $this->generatePathFeatures($methods)->toString();
-        echo $result;
+        $methodLength = count($methods);
+        foreach ($methods as $key => $method) {
+            // echo count($method)."\n";
+            if (count($method) >= 5000) {
+                continue;
+            }
+            $result = $this->generatePathFeatures($method)->toString();
+            if (strlen($result) <= 1) {
+                continue;
+            }
+            // echo "=====================".$key."/".$methodLength."====================="."\n";
+            // echo strlen($result)."\n";
+            echo $result;
+            if ($methodLength - 1 != $key) {
+                echo "\n";
+            }
+        }
+    }
+
+    /**
+     * @return Node[][]
+     */
+    public function parse($code)
+    {
+        $arr = [];
+        $mainMethodLeave = [];
+        $parser = new Parser();
+        $astNode = $parser->parseSourceFile($code);
+
+        $this->splitMethod($astNode);
+        if ($this->methods) {
+            foreach ($this->methods as $method) {
+                $maxCodeLength = 10000;
+                if (strlen($method->getText()) > $maxCodeLength) {
+                    continue;
+                }
+                $arr[] = $this->getLeaves($method);
+            }
+        }
+        if ($this->mainMethod) {
+            foreach ($this->mainMethod as $nodes) {
+                $leaves = $this->getLeaves($nodes);
+                foreach ($leaves as $leave) {
+                    $mainMethodLeave[] = $leave;
+                }
+            }
+        }
+
+        $arr[] = $mainMethodLeave;
+
+        return $arr;
     }
 
     /**
      * @return Node[]
      */
-    public function parse($code)
+    public function getLeaves(Node $node)
     {
         $arr = [];
-        $parser = new Parser();
-        $astNode = $parser->parseSourceFile($code);
-
-        foreach ($astNode->getDescendantNodes() as $descendant) {
+        $node->getText();
+        foreach ($node->getDescendantNodes() as $descendant) {
             $nodeType = $descendant->getNodeKindName();
             $childLength = count(iterator_to_array($descendant->getChildNodes(), false));
             if ($childLength === 0 && $nodeType != "exprInlineHtml" && $nodeType != "InlineHtml") {
                 $arr[] = $descendant;
             }
         }
-
         return $arr;
+    }
+
+    public function splitMethod(Node $astNode)
+    {
+        foreach ($astNode->getChildNodes() as $childNode) {
+            $nodeType = $childNode->getNodeKindName();
+            if ($nodeType == "FunctionDeclaration") {
+                $this->methods[] = $childNode;
+            } elseif ($nodeType == "ClassDeclaration") {
+                foreach ($childNode->getChildNodes() as $node) {
+                    if ($node->getNodeKindName() == "ClassMembersNode") {
+                        foreach ($node->getChildNodes() as $members) {
+                            $this->methods[] = $members;
+                        }
+                    }
+                }
+            } else {
+                $this->mainMethod[] = $childNode;
+            }
+        }
     }
 
     public function generatePathFeatures(array $methods)
@@ -92,6 +163,20 @@ class MSParser
             $commonPrefix++;
             $currentSourceAncestorIndex--;
             $currentTargetAncestorIndex--;
+        }
+
+        $pathLength = count($sourceStack) + count($targetStack) - 2 * $commonPrefix;
+        $maxPathLength = 8;
+        if ($pathLength > $maxPathLength) {
+            return $this->emptyString;
+        }
+
+        if ($currentSourceAncestorIndex >= 0 && $currentTargetAncestorIndex >= 0) {
+            $maxPathWidth = 2;
+            $pathWidth = $source->getWidth() - $target->getWidth();
+            if ($pathWidth > $maxPathWidth) {
+                return $this->emptyString;
+            }
         }
 
         for ($i = 0; $i < count($sourceStack) - $commonPrefix; $i++) {
